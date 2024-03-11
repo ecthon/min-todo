4970573d815610478d60a4a09e92d94c2cdc2d59
ecthon <ecthon@gmail.com>
2024-03-11 10:20:36 -0400

    feature: create header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <div className="flex flex-col w-[816px] h-screen mx-auto mt-10">
+      <header className="flex w-[604px] gap-5 items-center">
+        <div className="flex w-[80px] h-[80px] items-center justify-center bg-zinc-800">
+          <span className="font-bold text-4xl text-white">11</span>
+        </div>
+        <div className="flex flex-col gap-0">
+          <p className="font-bold text-4xl text-zinc-800">MAR</p>
+          <p className="">Segunda-feira, 2024</p>
+        </div>
+      </header>
+    </div>
   );
 }
